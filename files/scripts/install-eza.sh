2af25ad5d4b629fcc48161cee119bbbea4b66b47
Robert Ball <jhrobert@pm.me>
2025-12-10 13:40:33 -0800

    fixed other typo

--- files/scripts/install-eza.sh
+++ files/scripts/install-eza.sh
@@ -2,8 +2,8 @@
 
 set -oue pipefail
 
-curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/eza.tar.gz https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O -
-tar zx /tmp/eza.tar.gz -C /tmp
+curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/eza.tar.gz https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O
+tar zxf /tmp/eza.tar.gz -C /tmp
 sudo chmod +x /tmp/eza
 sudo chown root:root /tmp/eza
 sudo mv /tmp/eza /usr/local/bin/eza
